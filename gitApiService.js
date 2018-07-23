16bd4ace897fc7b62e3c6d98673c8636d312dc98
amashoshyna <a.mashoshyna@gmail.com>
2018-07-23 06:33:41 +0300

    refactor

--- gitApiService.js
+++ gitApiService.js
@@ -1,48 +1,55 @@
 const token = require("./token");
-const api = "https://api.github.com";
 const fetch = require("node-fetch");
+
+const API = "https://api.github.com";
+
+const getUserReposUrl = token => {
+    return `${API}/user/repos?access_token=${token}`;
+};
+
+const getGitBookUrl = (token, repo) => {
+    return `${API}/repos/${repo.owner.login}/${
+        repo.name
+    }/contents/book.json?access_token=${token}`;
+};
+
+const hasGitBook = token => repo => {
+    let url = getGitBookUrl(token, repo);
+    return fetch(url).then(response => {
+        if (response.ok) {
+            return repo;
+        }
+        return null;
+    });
+};
 
 class GitApiService {
     constructor(token) {
         this.token = token;
+        this.hasGitBook = hasGitBook(token);
     }
     getGitBookRepos() {
-        const url = `${api}/user/repos?access_token=${token}`;
-        return (
-            fetch(url)
-                .then(result => {
-                    if (result.ok) {
-                        return result.json();
-                    }
-                })
-                // .then(
-                //     repos => repos.filter(repo => repo.permissions.push)
-                //     // .map(repo => repo.name)
-                // )
-                .then(repos => {
-                    // console.log(repos);
-                    return Promise.all(
-                        repos.map(repo => {
-                            let url = `${api}/repos/${repo.owner.login}/${
-                                repo.name
-                            }/contents/book.json?access_token=${token}`;
-                            // console.log(url);
-                            return fetch(url).then(response => {
-                                if (response.ok) {
-                                    return repo;
-                                }
-                                return null;
-                            });
-                        })
-                    ).then(repos =>
-                        repos.filter(repo => repo).map(repo => ({
+        const url = getUserReposUrl(this.token);
+        return fetch(url)
+            .then(result => {
+                if (result.ok) {
+                    return result.json();
+                }
+            })
+            .then(
+                repos => repos.filter(repo => repo.permissions.push)
+                // .map(repo => repo.name)
+            )
+            .then(repos => {
+                return Promise.all(repos.map(this.hasGitBook)).then(repos =>
+                    repos.filter(repo => repo).map(repo => {
+                        return {
                             name: repo.name,
-                            url: repo.url
-                        }))
-                    );
-                    // .then(console.log);
-                })
-        );
+                            url: repo.html_url
+                        };
+                    })
+                );
+            });
     }
 }
 
