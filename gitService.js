6f0e0789f04a2e892f92695bd4470961a2d77d5c
amashoshyna <a.mashoshyna@gmail.com>
2018-07-23 06:34:57 +0300

    exten checkout method

--- gitService.js
+++ gitService.js
@@ -1,9 +1,18 @@
 const git = require("simple-git")();
+const mkdirp = require("mkdirp-promise");
 const token = require("./token");
 
 class GitService {
     checkout({ name, url }) {
-        return git.cwd(`./raw`).clone(url, "./");
+        return mkdirp(`./raw/${name}`).then(() =>
+            git.cwd(`./raw/${name}`).checkIsRepo((err, isRepo) => {
+                if (!isRepo) {
+                    return git.clone(url, "./");
+                } else {
+                    return git.fetch();
+                }
+            })
+        );
     }
 }
 
